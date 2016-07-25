1ca759252d1f8e25dba3a87da2063422428eb06f
Your Name <you@example.com>
2016-07-25 15:06:42 +0900

    -- first commit

--- 02_activity/app/src/main/java/com/example/a/a02_activity/MainActivity.java
+++ 02_activity/app/src/main/java/com/example/a/a02_activity/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.a.a02_activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onLoginClick(View v){
+        EditText myEdit = (EditText) findViewById(R.id.editID);
+        String id = myEdit.getText().toString();
+
+        // 잠시 몇 초동안 잠시 보여줌
+        Toast.makeText(MainActivity.this, "id : " + id, Toast.LENGTH_SHORT).show();
     }
 }
