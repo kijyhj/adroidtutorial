57501829262d1d33be7bf52e695cbdff6636e045
Your Name <you@example.com>
2016-07-25 17:51:42 +0900

    -- first commit

--- 04_widget2/app/src/main/java/com/example/a/a04_widget2/MainActivity.java
+++ 04_widget2/app/src/main/java/com/example/a/a04_widget2/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +24,31 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "on Button  Click", Toast.LENGTH_SHORT).show();
             }
         });
+
         CheckBox myCheckbox = (CheckBox) findViewById(R.id.myCheckBox);
+        myCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean b) {
+                Toast.makeText(MainActivity.this, "checked : " + b, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RadioGroup myGroup = (RadioGroup)findViewById(R.id.myRadioGroupup);
+        myGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int i) {
+                switch (i){
+                    case R.id.radio1:
+                        Toast.makeText(MainActivity.this, "radio1 selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radio2:
+                        Toast.makeText(MainActivity.this, "radio2 selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radio3:
+                        Toast.makeText(MainActivity.this, "radio3 selected", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
     }
 }
