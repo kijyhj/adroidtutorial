78dcd8df872453b0ca08bc4bbf9381afb148f573
Your Name <you@example.com>
2016-07-25 17:20:40 +0900

    -- first commit

--- 04_widget2/app/src/main/java/com/example/a/a04_widget2/MainActivity.java
+++ 04_widget2/app/src/main/java/com/example/a/a04_widget2/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.a.a04_widget2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnOk = (Button) findViewById(R.id.btnOk);
+        btnOk.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "on Button  Click", Toast.LENGTH_SHORT).show();
+            }
+        });
+        CheckBox myCheckbox = (CheckBox) findViewById(R.id.myCheckBox);
     }
 }
