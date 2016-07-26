796d6b7eb30ea91791298260e2ed4e5485e12a84
Your Name <you@example.com>
2016-07-26 12:09:46 +0900

    -- first commit

--- 08_thread/app/src/main/java/com/example/a/a08_thread/MainActivity.java
+++ 08_thread/app/src/main/java/com/example/a/a08_thread/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.a.a08_thread;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    ProgressBar progressBar;
 
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if(msg.what == MY_THREAD){
+                textView.setText("Count : " + msg.arg1);
+                progressBar.setProgress(msg.arg1);
+            }
+        }
+    };
+
+    private static final int MY_THREAD = 100;
     class MyThread extends Thread{
         @Override
         public void run() {
@@ -19,7 +35,10 @@ public class MainActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
                 Log.d("thread", "count : " + i);
-
+                Message msg = new Message();
+                msg.what = MY_THREAD;
+                msg.arg1 = i;
+                handler.sendMessage(msg);
             }
         }
     }
@@ -28,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.textView);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
         MyThread th = new MyThread();
         th.start();
     }
