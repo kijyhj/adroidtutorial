f98fab2946892e140e52e94014c159a777aedb88
Your Name <you@example.com>
2016-07-26 14:00:49 +0900

    -- first commit

--- 09_asyncTask/app/src/main/java/com/example/a/a09_asynctask/MainActivity.java
+++ 09_asyncTask/app/src/main/java/com/example/a/a09_asynctask/MainActivity.java
@@ -3,14 +3,28 @@ package com.example.a.a09_asynctask;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     class MyAsyncTask extends AsyncTask<Integer, Integer, String>{
 
         @Override
         protected String doInBackground(Integer... params) {
             int startValue = params[0];
+
+            for(int i = startValue ; i < 100 ; i++){
+                Log.d("asyncTask", "count : " + i);
+
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
 
             return null;
         }
@@ -20,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.textView);
 
         MyAsyncTask task = new MyAsyncTask();
         task.execute(30);
