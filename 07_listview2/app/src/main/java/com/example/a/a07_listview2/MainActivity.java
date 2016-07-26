26b2f8918d758795b043bf30654ffc8c6699a572
Your Name <you@example.com>
2016-07-26 10:54:04 +0900

    -- first commit

--- 07_listview2/app/src/main/java/com/example/a/a07_listview2/MainActivity.java
+++ 07_listview2/app/src/main/java/com/example/a/a07_listview2/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.a.a07_listview2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     class MyData{
@@ -10,12 +12,22 @@ public class MainActivity extends AppCompatActivity {
         String strTitle;
         String strDesc;
 
-
+        public MyData(int imgId, String strTitle, String strDesc) {
+            this.imgId = imgId;
+            this.strTitle = strTitle;
+            this.strDesc = strDesc;
+        }
     }
+
+    ArrayList<MyData> list = new ArrayList<MyData>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for(int i = 0 ; i < 30 ; i++){
+            list.add(new MyData(R.mipmap.ic_launcher, "title" + i, "desc" + i));
+        }
     }
 }
