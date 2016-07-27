55750be2176709ec2fee9263af2e7af05659414c
Your Name <you@example.com>
2016-07-27 14:39:51 +0900

    -- first commit

--- 15_mediaPlayer/app/src/main/java/com/example/a/a15_mediaplayer/MainActivity.java
+++ 15_mediaPlayer/app/src/main/java/com/example/a/a15_mediaplayer/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.a.a15_mediaplayer;
 
+import android.media.MediaPlayer;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String path = Environment.getExternalStorageDirectory() + "/Download/kam.mp3";
+
+        mp = new MediaPlayer();
+
+        try {
+            mp.setDataSource(path);
+            mp.prepare();
+            mp.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mp!=null){
+            mp.stop();
+            mp.release();
+            mp = null;
+        }
     }
 }
