bbeef519c2acc23ba2d68caccec61266b8d39efa
Your Name <you@example.com>
2016-07-27 16:56:21 +0900

    -- first commit

--- 17_service2/app/src/main/java/com/example/a/a17_service2/MainActivity.java
+++ 17_service2/app/src/main/java/com/example/a/a17_service2/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.a.a17_service2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    MyService myService;
+    boolean isBound = false;
+
+    ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MyService.MyBinder binder = (MyService.MyBinder) service;
+            myService = binder.getService();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            isBound = false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, MyService.class);
+        bindService(intent, connection, BIND_AUTO_CREATE);
+    }
+
+    public void onBtnClick(View v){
+        int num = myService.getRanDomNumber();
+
+        Toast.makeText(MainActivity.this, "num : " + num, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(isBound){
+            unbindService(connection);
+        }
     }
 }
