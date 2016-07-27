f2db50262310dc6bb3f6a7227f7a8f047494495e
Your Name <you@example.com>
2016-07-27 15:51:24 +0900

    -- first commit

--- 15_mediaPlayer/app/src/main/java/com/example/a/a15_mediaplayer/MainActivity.java
+++ 15_mediaPlayer/app/src/main/java/com/example/a/a15_mediaplayer/MainActivity.java
@@ -1,21 +1,49 @@
 package com.example.a.a15_mediaplayer;
 
 import android.media.MediaPlayer;
+import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mp;
+    SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser){
+                    if(mp != null){
+                        mp.seekTo(progress);
+                    }
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void onBtnPalyClick(View v){
         String path = Environment.getExternalStorageDirectory() + "/Download/kam.mp3";
 
         mp = new MediaPlayer();
@@ -24,8 +52,33 @@ public class MainActivity extends AppCompatActivity {
             mp.setDataSource(path);
             mp.prepare();
             mp.start();
+
+            seekBar.setMax(mp.getDuration());
+
+            Thread ph  = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (mp != null){
+                        try {
+                            seekBar.setProgress(mp.getCurrentPosition());
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+            });
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void onBtnStopClick(View v){
+        if(mp!=null){
+            mp.stop();
+            mp.release();
+            mp = null;
         }
     }
 
