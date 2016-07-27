1a051ca1481155d0c655aeee105fc84d5d1f1427
Your Name <you@example.com>
2016-07-27 10:35:16 +0900

    -- first commit

--- 12_sharePreference/app/src/main/java/com/example/a/a12_sharepreference/MainActivity.java
+++ 12_sharePreference/app/src/main/java/com/example/a/a12_sharepreference/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.a.a12_sharepreference;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         SharedPreferences preferences = getSharedPreferences("MySetting", 0);
+        String value = preferences.getString("name", "NO DATA");
+
+        Toast.makeText(MainActivity.this, "VALUE : " +value, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        SharedPreferences preferences =  getSharedPreferences("MySetting", 0);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        editor.putString("name", "이름");
+        editor.commit();
     }
 }
