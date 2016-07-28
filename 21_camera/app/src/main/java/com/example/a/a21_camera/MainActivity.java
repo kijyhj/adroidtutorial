a3e20b741f26f358e400f8f8f91cbc7928a0a0c3
Your Name <you@example.com>
2016-07-28 11:12:26 +0900

    --first commit

--- 21_camera/app/src/main/java/com/example/a/a21_camera/MainActivity.java
+++ 21_camera/app/src/main/java/com/example/a/a21_camera/MainActivity.java
@@ -1,7 +1,17 @@
 package com.example.a.a21_camera;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private static final int TAKE_PICTURE = 100;
+    public void onBtnClick(View v){
+        String path = Environment.getExternalStorageDirectory() + "/t21.jpg";
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(path)));
+        startActivityForResult(intent, TAKE_PICTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == TAKE_PICTURE){
+            if(resultCode == RESULT_OK){
+                String path = Environment.getExternalStorageDirectory() + "/t21.jpg";
+                ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                // 사진사이즈가 클 경우
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 4;
+                Bitmap bitmap = BitmapFactory.decodeFile(path, options);
+                imageView.setImageBitmap(bitmap);
+            }
+        }
     }
 }
