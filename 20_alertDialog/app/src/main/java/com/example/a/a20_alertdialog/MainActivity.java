5023ac1d975f3008925c83da1a43d02c09e64c26
Your Name <you@example.com>
2016-07-28 09:57:36 +0900

    -- first commit

--- 20_alertDialog/app/src/main/java/com/example/a/a20_alertdialog/MainActivity.java
+++ 20_alertDialog/app/src/main/java/com/example/a/a20_alertdialog/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.a.a20_alertdialog;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private static final int MY_DIALOG = 100;
+
+    public void onBtnClick(View v){
+        showDialog(MY_DIALOG);
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if(id == MY_DIALOG){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            builder.setTitle("TITLE");
+            builder.setMessage("MESSAGE");
+
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+
+            builder.setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+
+            return builder.create();
+        }
+        return super.onCreateDialog(id);
     }
 }
