879bbd166acd7629a99b619d683569bb96673647
Your Name <you@example.com>
2016-07-28 09:39:27 +0900

    -- first commit

--- 19_br2/app/src/main/java/com/example/a/a19_br2/MainActivity.java
+++ 19_br2/app/src/main/java/com/example/a/a19_br2/MainActivity.java
@@ -5,8 +5,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,10 +15,20 @@ public class MainActivity extends AppCompatActivity {
     BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
-            Toast.makeText(MainActivity.this, "batt : " + level, Toast.LENGTH_SHORT).show();
+            if(intent.getAction().equals(Intent.ACTION_BATTERY_CHANGED)) {
+                int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+                Toast.makeText(MainActivity.this, "batt : " + level, Toast.LENGTH_SHORT).show();
+            }else if(intent.getAction().equals("abcdefg")){
+                Toast.makeText(MainActivity.this, "abcdefg", Toast.LENGTH_SHORT).show();
+
+            }
         }
     };
+
+    public  void onBtnClick(View v){
+        Intent intent = new Intent("abcdefg");
+        sendBroadcast(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        filter.addAction(Intent.ACTION_BATTERY_LOW);
         registerReceiver(receiver, filter);
     }
 
