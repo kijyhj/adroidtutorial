002855510922e3cffb96b4fb88a27b4b5ee097a1
Your Name <you@example.com>
2016-07-28 16:32:29 +0900

    -- first commit

---
+++ p02_mp3Player/app/src/main/java/com/example/a/p02_mp3player/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.a.p02_mp3player;
+
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String path = Environment.getExternalStorageDirectory() + "/Music";
+
+    }
+}
